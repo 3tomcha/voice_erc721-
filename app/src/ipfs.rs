79e8467db00e74bed020ac614010b9be6786907e
Tomoya Kobayashi <tomocha.marika@gmail.com>
2022-10-28 07:30:57 +0900

    succeeded to up ipfs(needed ipfs daemon)

---
+++ app/src/ipfs.rs
@@ -0,0 +1,13 @@
+use ipfs_api_backend_hyper::{IpfsApi, IpfsClient};
+use std::io::Cursor;
+
+#[tokio::main]
+async fn main() {
+    let client = IpfsClient::default();
+    let data = Cursor::new("Hello World!");
+
+    match client.add(data).await {
+        Ok(res) => println!("{}", res.hash),
+        Err(e) => eprintln!("error adding file: {}", e)
+    }
+}
