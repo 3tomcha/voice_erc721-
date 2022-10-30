a1f2b8349c1c7a7ff0a6e8799b89dbaca7c878da
Tomoya Kobayashi <tomocha.marika@gmail.com>
2022-10-30 12:01:56 +0900

    add attributes

--- app/src/attributes_ipfs.rs
+++ app/src/attributes_ipfs.rs
@@ -1,10 +1,19 @@
 use ipfs_api_backend_hyper::{IpfsApi, IpfsClient};
 use std::io::Cursor;
+use serde_json::json;
 
 #[tokio::main]
 async fn main() {
     let client = IpfsClient::default();
-    let data = Cursor::new("Hello Worldavavsawaerabag!");
+    let john = json!({
+        "attributes": [
+            {
+                "trait_type": "name",
+                "value": "Tomoya Kobayashi"
+            }
+        ]
+        });
+    let data = Cursor::new(john.to_string());
 
     match client.add(data).await {
         Ok(res) => println!("{}", res.hash),
