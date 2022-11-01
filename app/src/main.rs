f8081f58d0c79b610b67600d344566c863ff4373
Tomoya Kobayashi <tomocha.marika@gmail.com>
2022-11-01 17:06:09 +0900

    ひとまずownerが表示できる状態

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,472 +1,65 @@
 use yew::prelude::*;
-use ethers::{prelude::*};
-use ethers_core::{
-    abi::Abi,
-    types:: {Address, H256}
-};
+use ethers::{ prelude::* };
+use ethers_core::{ abi::Abi, types::{ Address, H256 } };
 use std::fs::File;
+use std::io::BufReader;
 use std::io::prelude::*;
 use ethers_contract::Contract;
 use web_sys::console;
 use wasm_bindgen_futures::spawn_local;
 
-
-#[function_component(App)]
-fn app() -> Html {
-    async fn my_async_fn() -> String { String::from("hello") }
-
-    spawn_local(async {
-        let mut string = my_async_fn().await;
-        string.push_str(", world!");
-        // console log "Hello, world!"
-        console::log_1(&string.into());
-    });
-    // let result = my_async_fn();
-    html! {
-        <h1>{ "with text aaa" } </h1>
-    }
-}
-
 // #[function_component(App)]
-// async fn app() -> Html {
-//     let addr = "0x1261b8535678D59d1AeC7757aD3fbAE87A35F0FD".parse::<Address>().expect("error");
-//     let client = Provider::<Http>::try_from(
-//         "https://eth-goerli.g.alchemy.com/v2/lQQcdlj4Fye1AKw5R94wVNA-BDKevm0W"
-//     ).unwrap();
-//     let abi: Abi = serde_json::from_str(r#"
-//     [
-// 	{
-// 		"inputs": [
-// 			{
-// 				"internalType": "string",
-// 				"name": "name",
-// 				"type": "string"
-// 			},
-// 			{
-// 				"internalType": "string",
-// 				"name": "symbol",
-// 				"type": "string"
-// 			}
-// 		],
-// 		"stateMutability": "nonpayable",
-// 		"type": "constructor"
-// 	},
-// 	{
-// 		"anonymous": false,
-// 		"inputs": [
-// 			{
-// 				"indexed": true,
-// 				"internalType": "address",
-// 				"name": "owner",
-// 				"type": "address"
-// 			},
-// 			{
-// 				"indexed": true,
-// 				"internalType": "address",
-// 				"name": "approved",
-// 				"type": "address"
-// 			},
-// 			{
-// 				"indexed": true,
-// 				"internalType": "uint256",
-// 				"name": "tokenId",
-// 				"type": "uint256"
-// 			}
-// 		],
-// 		"name": "Approval",
-// 		"type": "event"
-// 	},
-// 	{
-// 		"anonymous": false,
-// 		"inputs": [
-// 			{
-// 				"indexed": true,
-// 				"internalType": "address",
-// 				"name": "owner",
-// 				"type": "address"
-// 			},
-// 			{
-// 				"indexed": true,
-// 				"internalType": "address",
-// 				"name": "operator",
-// 				"type": "address"
-// 			},
-// 			{
-// 				"indexed": false,
-// 				"internalType": "bool",
-// 				"name": "approved",
-// 				"type": "bool"
-// 			}
-// 		],
-// 		"name": "ApprovalForAll",
-// 		"type": "event"
-// 	},
-// 	{
-// 		"inputs": [
-// 			{
-// 				"internalType": "address",
-// 				"name": "to",
-// 				"type": "address"
-// 			},
-// 			{
-// 				"internalType": "uint256",
-// 				"name": "tokenId",
-// 				"type": "uint256"
-// 			}
-// 		],
-// 		"name": "approve",
-// 		"outputs": [],
-// 		"stateMutability": "nonpayable",
-// 		"type": "function"
-// 	},
-// 	{
-// 		"inputs": [
-// 			{
-// 				"internalType": "address",
-// 				"name": "recipient",
-// 				"type": "address"
-// 			},
-// 			{
-// 				"internalType": "string",
-// 				"name": "tokenURI",
-// 				"type": "string"
-// 			}
-// 		],
-// 		"name": "mintNFT",
-// 		"outputs": [
-// 			{
-// 				"internalType": "uint256",
-// 				"name": "",
-// 				"type": "uint256"
-// 			}
-// 		],
-// 		"stateMutability": "nonpayable",
-// 		"type": "function"
-// 	},
-// 	{
-// 		"anonymous": false,
-// 		"inputs": [
-// 			{
-// 				"indexed": true,
-// 				"internalType": "address",
-// 				"name": "previousOwner",
-// 				"type": "address"
-// 			},
-// 			{
-// 				"indexed": true,
-// 				"internalType": "address",
-// 				"name": "newOwner",
-// 				"type": "address"
-// 			}
-// 		],
-// 		"name": "OwnershipTransferred",
-// 		"type": "event"
-// 	},
-// 	{
-// 		"inputs": [],
-// 		"name": "renounceOwnership",
-// 		"outputs": [],
-// 		"stateMutability": "nonpayable",
-// 		"type": "function"
-// 	},
-// 	{
-// 		"inputs": [
-// 			{
-// 				"internalType": "address",
-// 				"name": "from",
-// 				"type": "address"
-// 			},
-// 			{
-// 				"internalType": "address",
-// 				"name": "to",
-// 				"type": "address"
-// 			},
-// 			{
-// 				"internalType": "uint256",
-// 				"name": "tokenId",
-// 				"type": "uint256"
-// 			}
-// 		],
-// 		"name": "safeTransferFrom",
-// 		"outputs": [],
-// 		"stateMutability": "nonpayable",
-// 		"type": "function"
-// 	},
-// 	{
-// 		"inputs": [
-// 			{
-// 				"internalType": "address",
-// 				"name": "from",
-// 				"type": "address"
-// 			},
-// 			{
-// 				"internalType": "address",
-// 				"name": "to",
-// 				"type": "address"
-// 			},
-// 			{
-// 				"internalType": "uint256",
-// 				"name": "tokenId",
-// 				"type": "uint256"
-// 			},
-// 			{
-// 				"internalType": "bytes",
-// 				"name": "data",
-// 				"type": "bytes"
-// 			}
-// 		],
-// 		"name": "safeTransferFrom",
-// 		"outputs": [],
-// 		"stateMutability": "nonpayable",
-// 		"type": "function"
-// 	},
-// 	{
-// 		"inputs": [
-// 			{
-// 				"internalType": "address",
-// 				"name": "operator",
-// 				"type": "address"
-// 			},
-// 			{
-// 				"internalType": "bool",
-// 				"name": "approved",
-// 				"type": "bool"
-// 			}
-// 		],
-// 		"name": "setApprovalForAll",
-// 		"outputs": [],
-// 		"stateMutability": "nonpayable",
-// 		"type": "function"
-// 	},
-// 	{
-// 		"anonymous": false,
-// 		"inputs": [
-// 			{
-// 				"indexed": true,
-// 				"internalType": "address",
-// 				"name": "from",
-// 				"type": "address"
-// 			},
-// 			{
-// 				"indexed": true,
-// 				"internalType": "address",
-// 				"name": "to",
-// 				"type": "address"
-// 			},
-// 			{
-// 				"indexed": true,
-// 				"internalType": "uint256",
-// 				"name": "tokenId",
-// 				"type": "uint256"
-// 			}
-// 		],
-// 		"name": "Transfer",
-// 		"type": "event"
-// 	},
-// 	{
-// 		"inputs": [
-// 			{
-// 				"internalType": "address",
-// 				"name": "from",
-// 				"type": "address"
-// 			},
-// 			{
-// 				"internalType": "address",
-// 				"name": "to",
-// 				"type": "address"
-// 			},
-// 			{
-// 				"internalType": "uint256",
-// 				"name": "tokenId",
-// 				"type": "uint256"
-// 			}
-// 		],
-// 		"name": "transferFrom",
-// 		"outputs": [],
-// 		"stateMutability": "nonpayable",
-// 		"type": "function"
-// 	},
-// 	{
-// 		"inputs": [
-// 			{
-// 				"internalType": "address",
-// 				"name": "newOwner",
-// 				"type": "address"
-// 			}
-// 		],
-// 		"name": "transferOwnership",
-// 		"outputs": [],
-// 		"stateMutability": "nonpayable",
-// 		"type": "function"
-// 	},
-// 	{
-// 		"inputs": [
-// 			{
-// 				"internalType": "address",
-// 				"name": "owner",
-// 				"type": "address"
-// 			}
-// 		],
-// 		"name": "balanceOf",
-// 		"outputs": [
-// 			{
-// 				"internalType": "uint256",
-// 				"name": "",
-// 				"type": "uint256"
-// 			}
-// 		],
-// 		"stateMutability": "view",
-// 		"type": "function"
-// 	},
-// 	{
-// 		"inputs": [
-// 			{
-// 				"internalType": "uint256",
-// 				"name": "tokenId",
-// 				"type": "uint256"
-// 			}
-// 		],
-// 		"name": "getApproved",
-// 		"outputs": [
-// 			{
-// 				"internalType": "address",
-// 				"name": "",
-// 				"type": "address"
-// 			}
-// 		],
-// 		"stateMutability": "view",
-// 		"type": "function"
-// 	},
-// 	{
-// 		"inputs": [
-// 			{
-// 				"internalType": "address",
-// 				"name": "owner",
-// 				"type": "address"
-// 			},
-// 			{
-// 				"internalType": "address",
-// 				"name": "operator",
-// 				"type": "address"
-// 			}
-// 		],
-// 		"name": "isApprovedForAll",
-// 		"outputs": [
-// 			{
-// 				"internalType": "bool",
-// 				"name": "",
-// 				"type": "bool"
-// 			}
-// 		],
-// 		"stateMutability": "view",
-// 		"type": "function"
-// 	},
-// 	{
-// 		"inputs": [],
-// 		"name": "name",
-// 		"outputs": [
-// 			{
-// 				"internalType": "string",
-// 				"name": "",
-// 				"type": "string"
-// 			}
-// 		],
-// 		"stateMutability": "view",
-// 		"type": "function"
-// 	},
-// 	{
-// 		"inputs": [],
-// 		"name": "owner",
-// 		"outputs": [
-// 			{
-// 				"internalType": "address",
-// 				"name": "",
-// 				"type": "address"
-// 			}
-// 		],
-// 		"stateMutability": "view",
-// 		"type": "function"
-// 	},
-// 	{
-// 		"inputs": [
-// 			{
-// 				"internalType": "uint256",
-// 				"name": "tokenId",
-// 				"type": "uint256"
-// 			}
-// 		],
-// 		"name": "ownerOf",
-// 		"outputs": [
-// 			{
-// 				"internalType": "address",
-// 				"name": "",
-// 				"type": "address"
-// 			}
-// 		],
-// 		"stateMutability": "view",
-// 		"type": "function"
-// 	},
-// 	{
-// 		"inputs": [
-// 			{
-// 				"internalType": "bytes4",
-// 				"name": "interfaceId",
-// 				"type": "bytes4"
-// 			}
-// 		],
-// 		"name": "supportsInterface",
-// 		"outputs": [
-// 			{
-// 				"internalType": "bool",
-// 				"name": "",
-// 				"type": "bool"
-// 			}
-// 		],
-// 		"stateMutability": "view",
-// 		"type": "function"
-// 	},
-// 	{
-// 		"inputs": [],
-// 		"name": "symbol",
-// 		"outputs": [
-// 			{
-// 				"internalType": "string",
-// 				"name": "",
-// 				"type": "string"
-// 			}
-// 		],
-// 		"stateMutability": "view",
-// 		"type": "function"
-// 	},
-// 	{
-// 		"inputs": [
-// 			{
-// 				"internalType": "uint256",
-// 				"name": "tokenId",
-// 				"type": "uint256"
-// 			}
-// 		],
-// 		"name": "tokenURI",
-// 		"outputs": [
-// 			{
-// 				"internalType": "string",
-// 				"name": "",
-// 				"type": "string"
-// 			}
-// 		],
-// 		"stateMutability": "view",
-// 		"type": "function"
-// 	}
-// ]   
-//     "#).expect("error"); 
-//     let contract = Contract::new(addr, abi, client);
-//     let value = contract.method::<_, String>("owner", ()).expect("error").call().await.expect("error");
-//     let receipt = pending_tx.confirmations(6).await.expect("error");
-
+// fn app() -> Html {
+//     async fn my_async_fn() -> String { String::from("hello") }
+//     let string;
+//     spawn_local(async {
+//         string = my_async_fn().await;
+//         string.push_str(", world!");
+//         // console log "Hello, world!"
+//         // console::log_1(&string.into());
+//     });
+//     console::log_1(&string.into());
+//     // let result = my_async_fn();
 //     html! {
 //         <h1>{ "with text aaa" } </h1>
 //     }
 // }
+
+#[function_component(App)]
+fn app() -> Html {
+    let value = use_state(|| "");
+    {
+        let value = value.clone();
+        use_effect_with_deps(move |_| {
+            // let value = value;
+            spawn_local(async move {
+                let addr = "0x1261b8535678D59d1AeC7757aD3fbAE87A35F0FD"
+                    .parse::<Address>()
+                    .expect("error");
+                let client = Provider::<Http>
+                    ::try_from(
+                        "https://eth-goerli.g.alchemy.com/v2/lQQcdlj4Fye1AKw5R94wVNA-BDKevm0W"
+                    )
+                    .unwrap();
+                let file = File::open("./contract_abi.json").unwrap();
+                let reader = BufReader::new(file);
+                let abi: Abi = serde_json::from_reader(reader).unwrap();
+                let contract = Contract::new(addr, abi, client);
+                let owner = contract
+                    .method::<_, String>("owner", ())
+                    .expect("error")
+                    .call().await
+                    .expect("error").to_string();
+                console::log_1(&owner.into());
+                // value.set(owner);
+            });
+            || ()
+        }, ());
+    }
+    html! {
+        <h1>{ "aaa" }</h1>
+    }
+}
 
 fn main() {
     yew::start_app::<App>();
