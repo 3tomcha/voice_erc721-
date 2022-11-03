25cd1af48b22256102dbf0cff7c761d5f3b52c06
Tomoya Kobayashi <tomocha.marika@gmail.com>
2022-11-03 22:39:22 +0900

    ハマりちゅう

--- app/src/main.rs
+++ app/src/main.rs
@@ -10,13 +10,31 @@ use web_sys::Window;
 use wasm_bindgen_futures::spawn_local;
 use gloo_net::http::Request;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
 use ethers_providers::{ Ws };
 use std::future::Future;
+use mopa::Any;
+use gloo_utils::format::JsValueSerdeExt;
+
+// #[wasm_bindgen]
+// Provider::<Http>
+#[macro_use]
+extern crate mopa;
 
 #[wasm_bindgen(module = "/src/index.js")]
 extern "C" {
-    async fn detectEthereumProvider() -> wasm_bindgen::JsValue;
+    async fn detectEthereumProvider() -> JsValue;
 }
+
+// pub trait FromJsValue: WasmDescribe {
+//     type 
+// }
+
+// pub fn render() -> Provider<Http> {
+//     let provider: Provider<Http> = Vec::new();
+//     detectEthereumProvider(&provider);
+// }
+
 // extern "C" {
 //     async fn detectEthereumProvider() -> wasm_bindgen::JsValue;
 // }
@@ -37,17 +55,52 @@ extern "C" {
 //     static: native: Native;
 // }
 
-pub fn test() {
-    spawn_local(async {
-        let provider = detectEthereumProvider().await;
-        console::log_1(&provider);
-    });
-}
-
+// pub fn test() {
+//     spawn_local(async {
+//         let provider: Provider<Http> = detectEthereumProvider().await;
+//         let chainId = provider.request({method: "eth_chainId"}).await;
+//         // console::log_1(&provider);
+//         console::log_1(&chainId);
+//     });
+// }
 
 #[function_component(App)]
 fn app() -> Html {
-    test();
+    let value = use_state(|| "");
+    {
+        let value = value.clone();
+        use_effect_with_deps(move |_| {
+            // let value = value;
+            spawn_local(async move {
+                let addr = "0x6B54d1665a0199e910cFE8D40C2eeeA0111Fd51c"
+                    .parse::<Address>() 
+                    .expect("error");
+                let mut client = detectEthereumProvider().await;
+                let client2: mopa::Any = JsValueSerdeExt::into_serde(&client).unwrap();
+                mopafy!(client2);
+                // let client = provider::Provider::<Http>();
+                let abi: Abi = Request::get("https://raw.githubusercontent.com/3tomcha/voice_erc721-/master/app/src/contract_abi.json")
+                            .send()
+                            .await
+                            .unwrap()
+                            .json()
+                            .await
+                            .unwrap();
+                // console::log_1(&JsValue::from(abi.to_string()));
+                let mut contract = Contract::new(addr, abi, client2);
+                let receipient_address = "0x32a9E70324862ef7BF8bA7610AF701822ddE5364".parse::<Address>().expect("error");
+                let token_uri = String::from("https://gateway.pinata.cloud/ipfs/QmTFCG9UPu5gfa2edbXEZVcr6BLu8NLzV14DWCLsedNFUd");
+                contract
+                    .method::<_, String>("mintNFT", (receipient_address, token_uri))
+                    .expect("error")
+                    .send().await
+                    .expect("error");
+                // console::log_1(&JsValue::from(res));
+                // value.set(owner);
+            });
+            || ()
+        }, ());
+    }
     html! {
         <h1>{ "aaa" }</h1>
     }
